052bc9c8145c22f3c67a074f6489fbad2522700e
Felipe96912 <furlinhfmf@gmail.com>
2026-02-27 22:15:22 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,16 @@ app.use(express.json());
 app.use("/chamados", require("./routes/chamados"));
 app.use("/auth", require("./routes/auth"));
 
+const path = require('path');
+
+// 'frontend' é o nome da sua pasta onde está o index.html
+app.use(express.static(path.join(__dirname, '../frontend'))); 
+
+// Rota para servir o index.html na raiz do site
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend', 'index.html'));
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
